99d006322e3de975acdb5109f6437004542a8866
Sunjae Kim <scientist01@unist.ac.kr>
2024-04-25 18:04:35 +0900

    run.sh 변경

--- 20201317_ChooWoungjae/project/run.sh
+++ 20201317_ChooWoungjae/project/run.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
-
+# Run ‘cd milestone1’
 cd milestone1
-
+# Run 'mvn package' command.
 mvn package
-
+# run mongodb start
+mongod --fork --logpath /var/log/mongodb/mongod.log --dbpath /data/db
+# Run your code with Java commands.
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
