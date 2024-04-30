f0d1ce94a346cf7f077b134ee92be317a1932562
Sunjae Kim <scientist01@unist.ac.kr>
2024-04-30 12:04:15 +0900

    mongod 이중으로 켜지는 것 방지용 주석처리

--- 20201317_ChooWoungjae/project/run.sh
+++ 20201317_ChooWoungjae/project/run.sh
@@ -3,6 +3,6 @@ cd milestone1
 # Run 'mvn package' command.
 mvn package
 # run mongodb start
-mongod --fork --logpath /var/log/mongodb/mongod.log --dbpath /data/db
+# mongod --fork --logpath /var/log/mongodb/mongod.log --dbpath /data/db
 # Run your code with Java commands.
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
