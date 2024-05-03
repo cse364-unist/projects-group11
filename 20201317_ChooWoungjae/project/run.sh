b2ecf8c5961469a2e151ae694f505fe4762b9996
Sunjae Kim <scientist01@unist.ac.kr>
2024-05-03 22:10:31 +0900

    add git

--- 20201317_ChooWoungjae/project/run.sh
+++ 20201317_ChooWoungjae/project/run.sh
@@ -1,8 +1,13 @@
-# Run ‘cd milestone1’
+git clone https://github.com/cse364-unist/projects-group11
+
+cd projects-group11
+
+git checkout milestone2
+
 cd milestone1
-# Run 'mvn package' command.
+
+mvn jacoco:report
+
 mvn package
-# run mongodb start
-# mongod --fork --logpath /var/log/mongodb/mongod.log --dbpath /data/db
-# Run your code with Java commands.
+
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
