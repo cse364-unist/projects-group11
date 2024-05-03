b54181d9e9032e08c8a262d2a533789bd7a2c43d
Sunjae Kim <scientist01@unist.ac.kr>
2024-05-03 13:51:36 +0900

    change readfile method to indexOf, substring

--- 20201317_ChooWoungjae/project/milestone1/src/main/java/cse364/project/ReadFile.java
+++ 20201317_ChooWoungjae/project/milestone1/src/main/java/cse364/project/ReadFile.java
@@ -4,6 +4,20 @@ import java.io.*;
 import java.util.*;
 
 public class ReadFile {
+    public static List<String> splitByDelimiter(String input, String delimiter) {
+        List<String> result = new ArrayList<>();
+        int start = 0;
+        int end = input.indexOf(delimiter);
+        while (end != -1) {
+            result.add(input.substring(start, end));
+            start = end + delimiter.length();
+            end = input.indexOf(delimiter, start);
+        }
+        if (start <= input.length()) {
+            result.add(input.substring(start));
+        }
+        return result;
+    }
     public List<List<String>> readDAT(String path){
         List<List<String>> csvList = new ArrayList<List<String>>();
         File file = new File(path);
@@ -14,9 +28,7 @@ public class ReadFile {
         try {
             buffer = new BufferedReader(new FileReader(file));
             while ((line = buffer.readLine()) != null) {
-                List<String> ParseList = new ArrayList<String>();
-                String[] LineParse = line.split("::");
-                ParseList = Arrays.asList(LineParse);
+                List<String> ParseList = splitByDelimiter(line, "::");
                 csvList.add(ParseList);
             }
         } catch(FileNotFoundException ex){
