da6bf929209d305c50db32a29619076a94dd32ca
Sunjae Kim <scientist01@unist.ac.kr>
2024-05-03 13:48:29 +0900

    delete MovieRatingByDemographic

--- 20201317_ChooWoungjae/project/milestone1/src/main/java/cse364/project/RatingRepository.java
+++ 20201317_ChooWoungjae/project/milestone1/src/main/java/cse364/project/RatingRepository.java
@@ -8,13 +8,4 @@ import org.springframework.data.mongodb.repository.Aggregation;
 
 public interface RatingRepository extends MongoRepository<Rating, String> {
     List<Rating> findByMovieId(Long movieId);
-
-    @Aggregation(pipeline = {
-        "{ $lookup: { from: 'users', localField: 'userId', foreignField: 'userId', as: 'userDetails' } }",
-        "{ $unwind: '$userDetails' }",
-        "{ $group: { _id: { movieId: '$movieId', gender: '$userDetails.gender', age: '$userDetails.age' }, avgRating: { $avg: '$rating' } } }",
-        "{ $sort: { '_id.movieId': 1, '_id.gender': 1, '_id.age': 1 } }"
-    })
-    List<MovieRatingByDemographic> getAverageRatingsByMovieIdGenderAndAge();
-
 }
