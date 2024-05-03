16901c8c683de2190b208e853e550dbc2c1f6cf0
Sunjae Kim <scientist01@unist.ac.kr>
2024-05-03 22:14:28 +0900

    change directory

--- 20201317_ChooWoungjae/project/milestone1/src/main/java/cse364/project/RatingRepository.java
+++
@@ -1,8 +0,0 @@
-package cse364.project;
-
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RatingRepository extends MongoRepository<Rating, String> {
-    List<Rating> findByMovieId(Long movieId);
-}
