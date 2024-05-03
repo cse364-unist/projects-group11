bc0956acc7d33b2820ea06d39dfae276bb3f1a1a
Sunjae Kim <scientist01@unist.ac.kr>
2024-05-03 22:46:26 +0900

    run.sh git 추가 및 변경

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
+# git clone
 git clone https://github.com/cse364-unist/projects-group11
-
-cd projects-group11
-
-git checkout milestone2
-
-cd milestone1
-
-mvn jacoco:report
-
+# go into git repo and checkout milestone
+cd projects-group11 && git checkout milestone2 
+# change folder structure to not have projects-group11 as parent directory
+cd .. && mv projects-group11/milestone2 . 
+rm -rf projects-group11
+# go into milestone2 folder
+cd milestone2
+# make package first to get target folder
 mvn package
-
+# create jacoco report
+mvn jacoco:report
+# run jar file
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
