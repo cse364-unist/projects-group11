b2e831223a3795fec2a490f57e9328be4ae40299
Sunjae Kim <scientist01@unist.ac.kr>
2024-06-15 00:19:51 +0900

    톰캣 run.sh 추가

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 # git clone
 git clone https://github.com/cse364-unist/projects-group11
 # go into git repo and checkout milestone
-cd projects-group11 && git checkout milestone2 
+cd projects-group11 && git fetch && git checkout -b master
 # change folder structure to not have projects-group11 as parent directory
-cd .. && mv projects-group11/milestone2 . 
+cd ../ && mv projects-group11/milestone2 ./
+# heelo wodl
 rm -rf projects-group11
 # go into milestone2 folder
 cd milestone2
@@ -13,3 +14,9 @@ mvn package
 mvn jacoco:report
 # run jar file
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
+# tomcat apache
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
+mkdir -p tomcat && tar -zvxf apache-tomcat-9.0.89.tar.gz -C tomcat/ --strip-components=1
+cp target/cse364-project.war tomcat/webapps/
+cd tomcat/bin
+sh catalina.sh run
