ac300060b18a188e19728da51b8aab7c6d471686
Sunjae Kim <scientist01@unist.ac.kr>
2024-06-15 17:40:48 +0900

    run.sh 변경 frontend 삭제

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ git clone https://github.com/cse364-unist/projects-group11
 # go into git repo and checkout milestone
 cd projects-group11 && git fetch && git checkout -b master
 # change folder structure to not have projects-group11 as parent directory
-cd ../ && mv projects-group11/milestone2 ./ && mv projects-group11/frontend ./ && mv projects-group11/tomcat_configs ./
+cd ../ && mv projects-group11/milestone2 ./ && mv projects-group11/tomcat_configs ./
 rm -rf projects-group11
 # go into milestone2 folder
 cd milestone2
@@ -17,8 +17,7 @@ java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
 mkdir -p tomcat && tar -zvxf apache-tomcat-9.0.89.tar.gz -C tomcat/ --strip-components=1
 cp target/cse364-project.war tomcat/webapps/
-mv ../frontend/ tomcat/webapps/
 mv -f ../tomcat_configs/* tomcat/conf/
-rm -rf ../frontend ../tomcat_configs
+rm -rf ../tomcat_configs
 cd tomcat/bin
 sh catalina.sh run
