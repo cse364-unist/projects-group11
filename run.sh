45f8460a7c364f554c5a172429a6e5731adbc98d
Sunjae Kim <scientist01@unist.ac.kr>
2024-06-16 00:44:00 +0900

    run.sh 변경

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-# git clone
+
 git clone https://github.com/cse364-unist/projects-group11
-# go into git repo and checkout milestone
 cd projects-group11 && git fetch && git checkout -b master 
 cd ../ && mv projects-group11/milestone3 . && mv projects-group11/tomcat_configs . 
 rm -rf projects-group11
@@ -10,12 +9,11 @@ cd milestone3
 mvn package
 # create jacoco report
 mvn jacoco:report
-# tomcat apache
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
-mkdir -p tomcat && tar -zvxf apache-tomcat-9.0.89.tar.gz -C tomcat/ --strip-components=1
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.tar.gz
+mkdir -p tomcat && tar -zvxf apache-tomcat-10.1.24.tar.gz -C tomcat/ --strip-components=1
 cp target/cse364-project.war tomcat/webapps/
-# mv ../frontend/ tomcat/webapps/
+mv ../frontend/ tomcat/webapps/
 mv -f ../tomcat_configs/* tomcat/conf/
-rm -rf ../tomcat_configs
+rm -rf ../frontend/ ../tomcat_configs
 cd tomcat/bin
 sh catalina.sh run
