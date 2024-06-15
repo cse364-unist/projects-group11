2c2a6c7916c01c881d628dbe7f30cf03fce25de8
Sunjae Kim <scientist01@unist.ac.kr>
2024-06-16 00:50:46 +0900

    run.sh 변경2탄

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
-
+# build
 git clone https://github.com/cse364-unist/projects-group11
 cd projects-group11 && git fetch && git checkout -b master 
-cd ../ && mv projects-group11/milestone3 . && mv projects-group11/tomcat_configs . 
+cd ../ && mv projects-group11/milestone3 . && mv projects-group11/frontend . && mv projects-group11/tomcat_configs . 
 rm -rf projects-group11
-# go into milestone3 folder
 cd milestone3
-# make package first to get target folder
-mvn package
-# create jacoco report
-mvn jacoco:report
+mvn clean package
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.24/bin/apache-tomcat-10.1.24.tar.gz
 mkdir -p tomcat && tar -zvxf apache-tomcat-10.1.24.tar.gz -C tomcat/ --strip-components=1
 cp target/cse364-project.war tomcat/webapps/
