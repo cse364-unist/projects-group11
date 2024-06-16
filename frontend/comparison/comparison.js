14245709158ae891d9935255f8f7a9ba345befa3
KKIMMINSEOKK <k_ms3164@naver.com>
2024-06-16 18:34:54 +0900

    comparison 결과창 수정

--- frontend/comparison/comparison.js
+++ frontend/comparison/comparison.js
@@ -1,8 +1,35 @@
-const movie1 = JSON.parse(localStorage.getItem("comparison-movie1"));
-const movie2 = JSON.parse(localStorage.getItem("comparison-movie2"));
+let movie1 = JSON.parse(localStorage.getItem("comparison-movie1"));
+let movie2 = JSON.parse(localStorage.getItem("comparison-movie2"));
 console.log(movie1, movie2);
 
-let comparisonResults = [];
+// dummy data
+movie1 = {
+    title: "movie1",
+    movie_id: 1
+}
+movie2 = {
+    title: "movie2",
+    movie_id: 2
+}
+
+// load winner data
+function loadResult() {
+    // firstMovieTitle.append(DUMMY_DATA[0].title);
+    // secondMovieTitle.append(DUMMY_DATA[1].title);
+
+    // 이긴 영화 id 얻으면 movie1과 movie2의 id 비교한 후 그 영화의 title
+
+    winnerMovieTitle.append(winner);
+    // firstMovieTitle.append(movie1.title);
+    // secondMovieTitle.append(movie2.title);
+    firstMovieData.append(firstMovieDescription);
+    // secondMovieData.append(secondMovieDescription);
+}
+
+
+// let comparisonResults = [];
+let comparisonsResults = [];
+
 
 function fetchComparison() {
     const requestURL = 'http://localhost:8080/api/comparisons?id1=' + movie1.movie_id + '&id2=' + movie2.movie_id;
@@ -11,7 +38,7 @@ function fetchComparison() {
         type: "POST",
         url: requestURL, // 요청 url
         data: {},
-        success: function(response) {
+        success: function (response) {
             console.log(response);
             comparisonResults = response;
         },
@@ -25,6 +52,12 @@ function fetchComparison() {
 
 fetchComparison();
 
+// dummy data
+// comparisonResults = [1, -1, 1431, 569, 532, 761, 554, 153, 8475, 2077, -1, 409, 142, 139, 206, 162, 44, 1986, 701, -1, 4331, 1709, 1325, 2096, 1743, 876];
+
+
+const winner = movie1.movie_id === comparisonResults[0] ? movie1.title : movie2.title;
+
 // back to main page
 $('#back-button').on('click', function () {
     window.location.href = '../index.html';
@@ -32,9 +65,7 @@ $('#back-button').on('click', function () {
 
 const winnerMovieTitle = document.querySelector("#winner-movie");
 const firstMovieTitle = document.querySelector("#first-movie-title");
-const secondMovieTitle = document.querySelector("#second-movie-title");
 const firstMovieData = document.querySelector("#first-result");
-const secondMovieData = document.querySelector("#second-result");
 
 const maleRateOne = comparisonResults[2]/comparisonResults[20];
 const femaleRateOne = comparisonResults[3]/comparisonResults[21];
@@ -55,38 +86,23 @@ const sumAgeTwo = age1RateTwo + age25RateTwo + age35RateTwo + age50RateTwo;
 const ratingOne = comparisonResults[8]/comparisonResults[9];
 const ratingTwo = comparisonResults[17]/comparisonResults[18];
 
-let ratingWinnerTitle = (ratingOne > ratingTwo) ? firstMovieTitle : secondMovieTitle;
-let conjunction = (ratingWinnerTitle == winnerMovieTitle) ? "Also" : "However";
-let reason = (ratingWinnerTitle == winnerMovieTitle) ? "not powerful enough to weaken the rating of" : "much more even(means evenly favored) at";
+let ratingWinnerTitle = (ratingOne > ratingTwo) ? movie1.title : movie2.title;
+let conjunction = (ratingWinnerTitle == winner) ? "Also" : "However";
+let reason = (ratingWinnerTitle == winner) ? "not powerful enough to weaken the rating of" : "much more even(means evenly favored) at";
 
-let firstMovieDescription = `${winnerMovieTitle} won! The average rating is bigger at ${ratingWinnerTitle}. ${conjunction}, The distribution of users was ${reason} ${winnerMovieTitle}!`;
-let secondMovieDescription = "The reason why the movie win/lose";
-
-function loadResult() {
-    // firstMovieTitle.append(DUMMY_DATA[0].title);
-    // secondMovieTitle.append(DUMMY_DATA[1].title);
-
-    // 이긴 영화 id 얻으면 movie1과 movie2의 id 비교한 후 그 영화의 title
-    const winner = movie1.movie_id === comparisonResults[0] ? movie1.title : movie2.title;
-
-    winnerMovieTitle.append(winner);
-    firstMovieTitle.append(movie1.title);
-    secondMovieTitle.append(movie2.title);
-    firstMovieData.append(firstMovieDescription);
-    secondMovieData.append(secondMovieDescription);
-}
+let firstMovieDescription = `${winner} won! The average rating is bigger at ${ratingWinnerTitle}. ${conjunction}, The distribution of users was ${reason} ${winner}!`;
 
 var genderChartData = {
-    labels: ["Male", "Female"],
+    labels: [movie1.title, movie2.title],
     datasets: [{
-        label: movie1.title,
+        label: "Male",
         backgroundColor: "#1E90FF",
         data: [
             100 * maleRateOne / (maleRateOne + femaleRateOne),
             100 * femaleRateOne / (maleRateOne + femaleRateOne)
         ]
     }, {
-        label: movie2.title,
+        label: "Female",
         backgroundColor: "#F7464A",
         data: [
             100 * maleRateTwo / (maleRateTwo + femaleRateTwo),
@@ -96,24 +112,34 @@ var genderChartData = {
 };
 
 var ageChartData = {
-    labels: ["Under 25", "25~34", "35~49", "Over 50"],
+    labels: [movie1.title, movie2.title],
     datasets: [{
-        label: movie1.title,
+        label: "Under 25",
         backgroundColor: "#1E90FF",
         data: [
             100 * age1RateOne / sumAgeOne,
-            100 * age25RateOne / sumAgeOne,
-            100 * age35RateOne / sumAgeOne,
-            100 * age50RateOne / sumAgeOne
+            100 * age1RateTwo / sumAgeTwo,
         ]
     }, {
-        label: movie2.title,
+        label: "25-34",
         backgroundColor: "#F7464A",
         data: [
-            100 * age1RateTwo / sumAgeTwo,
             100 * age25RateTwo / sumAgeTwo,
+            100 * age25RateOne / sumAgeOne,
+        ]
+    }, {
+        label: "35-49",
+        backgroundColor: "green",
+        data: [
+            100 * age35RateOne / sumAgeOne,
             100 * age35RateTwo / sumAgeTwo,
-            100 * age50RateTwo / sumAgeTwo
+        ]
+    }, {
+        label: "Over 50",
+        backgroundColor: "yellow",
+        data: [
+            100 * age50RateOne / sumAgeOne,
+            100 * age50RateTwo / sumAgeTwo,
         ]
     }]
 };
@@ -123,15 +149,32 @@ window.onload = function () {
         type: 'bar',
         data: genderChartData,
         options: {
-
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        // suggestedMax: 53,
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
     });
+
     var ctx2 = $('#age-chart').get(0).getContext("2d");
     window.theChart = new Chart(ctx2, {
         type: 'bar',
         data: ageChartData,
         options: {
-
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        // suggestedMax: 53,
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
     });
 }
